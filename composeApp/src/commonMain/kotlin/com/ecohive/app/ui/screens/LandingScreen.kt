cb0ea166657c4ee9099202cffd202c8996b36272
Oana Topan <oanamariat1@gmail.com>
2025-03-11 21:05:16 +0200

    Setup project with working type safe navigation

---
+++ composeApp/src/commonMain/kotlin/com/ecohive/app/ui/screens/LandingScreen.kt
@@ -0,0 +1,32 @@
+package com.ecohive.app.ui.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun LandingScreen(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        TextButton(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth(0.5f).wrapContentHeight().align(Alignment.Center),
+            colors = ButtonDefaults.textButtonColors(backgroundColor = Color.Black)
+        ) {
+            Text("Landing Screen")
+        }
+
+    }
+}
